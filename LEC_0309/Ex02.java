f0fd266db6b3f40633e3a6bb3afb1227a7d0d987
Daria <dashazoueva@gmail.com>
2022-09-07 12:58:25 +0300

    m

--- LEC_0309/Ex02.java
+++ LEC_0309/Ex02.java
@@ -3,16 +3,20 @@ import java.util.logging.*;
 // import java.util.logging.Level;
 // import java.util.logging.Logger;
 // import java.util.logging.SimpleFormatter;
-public class prog {
+// import java.util.logging.FileHandler;
+
+public class Ex02 {
    public static void main(String[] args) {
-       Logger logger = Logger.getLogger(prog.class.getName());
+       Logger logger = Logger.getLogger(Ex02.class.getName());
        logger.setLevel(Level.INFO);
-       ConsoleHandler ch = new ConsoleHandler();
-       logger.addHandler(ch);
+    //    ConsoleHandler ch = new ConsoleHandler();
+    //    logger.addHandler(ch);
+        FileHandler fh = new FileHandler("log.xml");
+        logger.addHandler(fh);
       //  SimpleFormatter sFormat = new SimpleFormatter();
        XMLFormatter xml = new XMLFormatter();
       //  ch.setFormatter(sFormat);
-       ch.setFormatter(xml);
+       fh.setFormatter(xml);
        logger.log(Level.WARNING, "Тестовое логирование");
        logger.info("Тестовое логирование");
    }
